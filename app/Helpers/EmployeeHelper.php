ff12c924714de6773d136892186626b419c7599b
latief <la790x@gmail.com>
2023-04-10 14:50:08 +0700

    fixing url photo profile on topbar

--- app/Helpers/EmployeeHelper.php
+++ app/Helpers/EmployeeHelper.php
@@ -2,6 +2,21 @@
 
 namespace App\Helpers;
 
+use URL;
+use Storage;
+use App\Models\Employee;
+
 class EmployeeHelper
 {
+    public function getUrlPhotoByUserId($userId)
+    {
+        $result = URL::asset('/build/images/users/avatar-1.jpg');
+        $employee = Employee::where('user_id', $userId)->first();
+
+        if ($employee) {
+            $result = Storage::disk('employee-photo')->url($employee->photo);
+        }
+
+        return $result;
+    }
 }
--- resources/views/layouts/skote/topbar.blade.php
+++ resources/views/layouts/skote/topbar.blade.php
@@ -139,7 +139,7 @@
         <div class="dropdown d-inline-block">
             <button type="button" class="btn header-item waves-effect" id="page-header-user-dropdown"
                 data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                <img class="rounded-circle header-profile-user" src="{{ isset(Auth::user()->avatar) ? asset(Auth::user()->avatar) : asset('/build/images/users/avatar-1.jpg') }}"
+                <img class="rounded-circle header-profile-user" src="{{ app('employee.helper')->getUrlPhotoByUserId(Auth::user()->id) }}"
                     alt="Header Avatar">
                 <span class="d-none d-xl-inline-block ms-1" key="t-henry">{{ ucwords(Auth::user()->name) }}</span>
                 <i class="mdi mdi-chevron-down d-none d-xl-inline-block"></i>
